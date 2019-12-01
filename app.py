23c473de197bbe4e51e9cb04448108f5a19e17ee
Steven Nguyen <steven.nguyen89@gmail.com>
2019-12-01 10:58:55 -0500

    Hook up to DHT11 sensor

--- app.py
+++ app.py
@@ -1,37 +1,32 @@
 #!/usr/bin/env python
 
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
+import Adafruit_DHT
 import datetime
 import time
 
+DATA_FILE = 'data.csv'
+
+def log(temperature, humidity):
+    now = datetime.datetime.utcnow()
+    timestamp = now.strftime('%d-%b-%Y %H:%M:%S')
+
+    print(
+            f'{timestamp} >> Temp={temperature:0.1f}C Humdity={humidity:0.1f}%'
+        )
+    with open(DATA_FILE, 'a') as f:
+        f.write(f'{timestamp},{temperature},{humidity}\n')
+
 if __name__ == '__main__':
-    """
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
 
-    GPIO.setup(18, GPIO.OUT)
-    """
+    DHTSensor = Adafruit_DHT.DHT11
+    DATA_PIN = 23
 
-    i = 0
-
-    data_file = 'data.csv'
-    open(data_file, 'w').close()
-
+    open(DATA_FILE, 'w').close()
     print('Start logging temperature and humidity')
     while True:
-        now = datetime.datetime.utcnow()
-        timestamp = now.strftime('%d-%b-%Y %H:%M:%S')
-        humidity, temperature = (60.0, 27.0)
-
-        print(
-            f'{timestamp} >> Temp={temperature:0.1f}C Humdity={humidity:0.1f}%'
-        )
-        with open(data_file, 'a') as f:
-            f.write(f'{timestamp},{temperature},{humidity}\n')
-
-        time.sleep(1)
-
-        if i >= 3:
-            break
-
-        i += 1
+        humidity, temperature = Adafruit_DHT.read_retry(DHTSensor, DATA_PIN)
+        log(temperature, humidity)
+        time.sleep(5 * 60)
